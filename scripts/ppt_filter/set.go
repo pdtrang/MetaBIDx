87f7eb01eb230dacb0aceb1cd1ca7afc93b6cbe5
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-03-21 21:09:21 -0500

    add strategy3

--- scripts/ppt_filter/set.go
+++ scripts/ppt_filter/set.go
@@ -26,3 +26,21 @@ func (s *Set) Has(item int64) bool {
 }
 
 //-----------------------------------------------------------------------------
+type StringSet struct {
+	Table map[string]struct{}
+}
+
+func NewStringSet() *StringSet {
+	return &StringSet{Table: make(map[string]struct{})}
+}
+
+func (s *StringSet) Add(item string) {
+	s.Table[item] = struct{}{}
+} 
+
+func (s *StringSet) Has(item string) bool {
+	if _, ok := s.Table[item]; ok {
+		return true
+	}
+	return false
+}
