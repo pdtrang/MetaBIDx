114c3353244778b669e536d79a4ef8ab538565d8
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-09-26 23:58:25 -0500

    fix errors

--- src/ppt_filter/query_utils.go
+++ src/ppt_filter/query_utils.go
@@ -28,7 +28,7 @@ func SaveSignatures(f *Filter, signatures []int64, idx uint16, bacteria_map map[
 	return bac_found
 }
 
-func PrintOnlineResult(f *Filter, idx uint16, read_1 []byte, read_2 []byte, kmer []byte) {
+func PrintOnlineResult(f *Filter, idx uint16, read_1 []byte, read_2 []byte, kmer []byte, bacteria_map map[uint16]*Bacteria) {
 
 	if strings.Contains(string(read_1), string(kmer)) || strings.Contains(string(RevComp(string(read_1))), string(kmer)) {
 		fmt.Println("Kmer is in Read 1")
@@ -41,8 +41,10 @@ func PrintOnlineResult(f *Filter, idx uint16, read_1 []byte, read_2 []byte, kmer
 	}
 
 	fmt.Println("Kmer: ", string(kmer))
-	//fmt.Println("Strain: ", f.Gid[idx])
-	fmt.Println("-------------------------")	
+	fmt.Println("Strain: ", f.Gid[idx])
+	fmt.Println("Number of signature found: ", bacteria_map[idx].Signatures.Size()+1) 
+	fmt.Println("Threshold: ", bacteria_map[idx].UpperThreshold, bacteria_map[idx].LowerThreshold)
+	// fmt.Println("-------------------------")	
 }
 
 func RevComp(s string) (string){
--- src/ppt_filter/twophase_query.go
+++ src/ppt_filter/twophase_query.go
@@ -156,10 +156,9 @@ func (f *Filter) TwoPhaseOneOrNothingQuery(read_1 []byte, read_2 []byte, bacteri
 
 	idx, is_valid_gid, kmer := f.TwoPhasesOONQueryRead(read_1, &kmers, idx)
 	if is_valid_gid {
-		PrintOnlineResult(f, idx, read_1, read_2, kmer)
-		fmt.Println("idx: ", idx)
-		fmt.Println("Number of signature found: ", bacteria_map[idx].Signatures.Size()+1) 
-		fmt.Println("Threshold: ", bacteria_map[idx].UpperThreshold, bacteria_map[idx].LowerThreshold)
+		if idx != uint16(0) {
+			PrintOnlineResult(f, idx, read_1, read_2, kmer, bacteria_map)	
+		}
 		idx, is_valid_gid, kmer = f.TwoPhasesOONQueryRead(read_2, &kmers, idx)	
 	} else {
 		return 0
@@ -167,10 +166,7 @@ func (f *Filter) TwoPhaseOneOrNothingQuery(read_1 []byte, read_2 []byte, bacteri
 	
 	// if there is only one gid and that gid is not 0
 	if is_valid_gid && idx != uint16(0) {
-		PrintOnlineResult(f, idx, read_1, read_2, kmer)
-		fmt.Println("idx: ", idx)
-		fmt.Println("Number of signature found: ", bacteria_map[idx].Signatures.Size()+1) 
-		fmt.Println("Threshold: ", bacteria_map[idx].UpperThreshold, bacteria_map[idx].LowerThreshold)
+		PrintOnlineResult(f, idx, read_1, read_2, kmer, bacteria_map)
 		if analysis == true {
 			_, err := analysis_fi.WriteString(string(read_1)+ "," +string(read_2)+","+string(idx)+"\n")
 			if err != nil {
