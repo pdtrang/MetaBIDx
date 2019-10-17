d4ceb297d4c1f4c2ceace1cb2cd19b320ebf845b
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-10-17 01:20:47 -0500

    change print

--- src/ppt_filter/signature.go
+++ src/ppt_filter/signature.go
@@ -28,8 +28,7 @@ func (f *Filter) HashSignature(kmer []byte, is_first_kmer bool, gid uint16, ph i
 
 	// fmt.Println(len(idx))
 	if unique_to_genome {
-		fmt.Println("Phase = ", ph, ", kmer = ", string(kmer))
-		fmt.Print("Hash values = ")
+		fmt.Print("Phase = ", ph, ", kmer = ", string(kmer), "hash values = ")
 		for i := 0; i < len(idx); i++ {
 			fmt.Print(idx[i], "\t")
 			f.table[idx[i]] = gid
