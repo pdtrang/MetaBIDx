504f11f3723278eb01419ea4b28fa555c3814a5f
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-10-18 13:22:32 -0500

    mofify print

--- src/ppt_filter/query_utils.go
+++ src/ppt_filter/query_utils.go
@@ -94,7 +94,7 @@ func SaveQueryResult(f *Filter, bacteria_map map[uint16]*Bacteria, num_bacteria 
     // save all reported bacteria
 	for k, b := range bacteria_map {
     	if b.Reported == true {
-    		s := f.Gid[k] + "\t" + b.QueryTime.String()+ "\n"
+    		s := f.Gid[k] +  "\n"
     		_, err := fi.WriteString(s)
 		    if err != nil {
 		        fmt.Println(err)
