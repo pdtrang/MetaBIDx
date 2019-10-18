c8a631e46dc95452edb104ab89a729e32adcb83b
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-10-18 11:42:10 -0500

    remove print reade headers

--- src/ppt_filter/online_query_pairs.go
+++ src/ppt_filter/online_query_pairs.go
@@ -80,8 +80,8 @@ func (f *Filter) OnlinePairQuery(read_file_1 string, read_file_2 string, out_fil
 		c += 1
 		// fmt.Println(scanner.Seq)
 		// fmt.Println(scanner2.Seq)
-		fmt.Println(scanner.Header)
-		fmt.Println(scanner2.Header)
+		// fmt.Println(scanner.Header)
+		// fmt.Println(scanner2.Header)
 		if f.N_phases == 2 {
 			num_bacteria += f.TwoPhaseQuery([]byte(scanner.Seq), []byte(scanner2.Seq), bacteria_map, start_time, strategy, analysis, analysis_fi)						
 		} else if f.N_phases == 1 {
