2f2927ac507b9ae2317ecca8a3c9223041fd147b
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-10-20 02:02:20 -0500

    debug

--- src/test_online.go
+++ src/test_online.go
@@ -12,40 +12,31 @@ func main() {
 	filter_saved_file := flag.String("load", "", "filter saved file")
     read_1 := flag.String("r1", "", "fastq/fq file")
     read_2 := flag.String("r2", "", "fastq/fq file")
-    out := flag.String("out", "", "output filename")
+    out := flag.String("output", "", "output filename")
     strategy := flag.String("strategy", "majority", "querying strategy")
     ut := flag.Float64("ut", float64(0.5), "upper threshold")
     lt := flag.Float64("lt", float64(0.2), "lower threshold")
-    //analysis := flag.Bool("analysis", false, "save read query to file")
+    analysis := flag.Bool("analysis", false, "save read query to file")
     flag.Parse()
-	
+
+	// Load filter
+	log.Printf("Load filter")
+    f := ppt_filter.Load(*filter_saved_file)
+    // fmt.Println(f)
+    // fmt.Println(f.Gid)
+    // f.Summarize()	
+    log.Println("Finish loading filter.")
+
+    upper_threshold := *ut
+    lower_threshold := *lt
+
     log.Printf(*out)
-    log.Printf(*strategy)
-    log.Printf(string(*ut))
-    log.Printf(string(*lt))
-    log.Printf(*filter_saved_file)
-    log.Printf(*read_1)
-    log.Printf(*read_2)
-    log.Printf(*strategy)
-    //log.Printf(*analysis)
 
-
-	// // Load filter
-	// log.Printf("Load filter")
- //    f := ppt_filter.Load(*filter_saved_file)
- //    // fmt.Println(f)
- //    // fmt.Println(f.Gid)
- //    // f.Summarize()	
- //    log.Println("Finish loading filter.")
-
- //    upper_threshold := *ut
- //    lower_threshold := *lt
-
-	// if *read_2 == "" {
-	// 	f.OnlineSingleQuery(*read_1, *out, *strategy, upper_threshold, lower_threshold, *analysis)	
-	// } else {
-	// 	f.OnlinePairQuery(*read_1, *read_2, *out, *strategy, upper_threshold, lower_threshold, *analysis)
-	// }
+	if *read_2 == "" {
+		f.OnlineSingleQuery(*read_1, *out, *strategy, upper_threshold, lower_threshold, *analysis)	
+	} else {
+		f.OnlinePairQuery(*read_1, *read_2, *out, *strategy, upper_threshold, lower_threshold, *analysis)
+	}
 
 }
 
