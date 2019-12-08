5ac823dd6674ff6e6f5dc99c2867661099e2bb32
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-12-08 15:48:56 -0600

    main for new genome traverse

---
+++ src/fast_traversal.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "./ppt_filter"
+    "flag"
+    "fmt"
+    "os"
+    // "math"
+)
+
+//-----------------------------------------------------------------------------
+func Traverse(refseq string, k int, swindow int) {
+    // Walk through refseq dir 
+    fscaner := ppt_filter.NewFileScanner(refseq)
+
+    // Scan reference genomes
+    for fidx, filename := range fscaner.Scan() {
+        fmt.Println(fidx)
+        fa, err := os.Open(filename)
+        if err != nil {
+            panic(err)
+        }
+        fa_scanner := ppt_filter.NewFastaScanner(fa)
+        // Scan through all sequences in the fasta file
+        for fa_scanner.Scan() {
+
+            // Sequence header, and seq length            
+            fmt.Println(fa_scanner.Header[1:], len(fa_scanner.Seq))
+            fmt.Println(string(fa_scanner.Seq))
+            // fmt.Printf("%s,",fa_scanner.Header)
+            kmer_scanner := ppt_filter.NewKmerScannerSkip(fa_scanner.Seq, k, swindow)
+            for kmer_scanner.ScanBothStrandsWithSkippingWindow() {
+                fmt.Println(string(kmer_scanner.Kmer))
+                // fmt.Println()
+            }
+        }       
+    }
+
+}
+
+//-----------------------------------------------------------------------------
+func main() {
+
+    refseq_genomes := flag.String("refseq", "", "refseq genome dir")    
+    // K := flag.Int("k", 16, "kmer length")
+    
+    flag.Parse()
+    
+    K := 5
+    swindow := 4
+    Traverse(*refseq_genomes, K, swindow)
+    
+
+}
