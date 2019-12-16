c323ae1b86764c9bd7823922013d86bd49399610
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2019-12-15 18:23:39 -0600

    add analyze_filter

---
+++ src/analyze_filter.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"./ppt_filter"
+	"log"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	filter_saved_file := flag.String("load", "", "filter saved file")
+	flag.Parse()
+
+	log.Printf("Load filter")
+	f := ppt_filter.Load(*filter_saved_file)
+	log.Printf("Finish loading filter.")
+	// fmt.Println(f)
+    // fmt.Println(f.Gid)
+    for key, value := range f.Gid {
+	    fmt.Println(key,":",value)
+	}
+    f.Summarize()	
+    // f.PrintHashFunction()
+	//f.GetNumberOfUniqueKmers()
+	// fmt.Println(f.Gid)
+}
