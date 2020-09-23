fd329c2e01554e29eaee57a73ead693cd97dba48
pdtrang <phamdiemtrang1987@gmail.com>
2020-09-23 01:43:57 -0500

    remove unused function

--- src/ppt_filter/signature.go
+++ src/ppt_filter/signature.go
@@ -68,32 +68,3 @@ func Find(slice []int, val int) (int, bool) {
     return -1, false
 }
 
-func (f *Filter) HashSignatureWithWindow(kmer []byte, is_first_kmer bool, gid uint16, is_max_num_kmers bool) bool {
-	unique_to_genome := true
-	idx := make([]int64, 0)
-	for i := 0; i < len(f.HashFunction); i++ {
-		j := f.HashFunction[i].SlidingHashKmer(kmer, is_first_kmer)
-		idx = append(idx, j)
-		if f.table[j] != 0 && f.table[j] != gid {
-			unique_to_genome = false
-		}
-	}
-
-	if unique_to_genome {
-		for i := 0; i < len(idx); i++ {
-			if is_max_num_kmers {
-				f.table[idx[i]] = Dirty
-			} else {
-				f.table[idx[i]] = gid	
-			}
-		}
-	} else {
-		for i := 0; i < len(idx); i++ {
-			f.table[idx[i]] = Dirty
-		}
-	}
-
-	return unique_to_genome
-}
-
-
