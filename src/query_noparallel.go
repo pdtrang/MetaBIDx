d238dc47549777c8b0d429647966f84944e4c5e1
pdtrang <phamdiemtrang1987@gmail.com>
2021-01-25 00:20:01 -0600

    keep nonparallel code

---
+++ src/query_noparallel.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"./ppt_filter"
+    "./utils"
+	//"fmt"
+	"flag"
+	"log"
+    "time"
+    //"runtime"
+)
+
+func main() {
+	// get inputs from commandline
+	filter_saved_file := flag.String("load", "", "filter saved file")
+    read_1 := flag.String("r1", "", "fastq/fq file")
+    read_2 := flag.String("r2", "", "fastq/fq file")
+    out := flag.String("out", "result.txt", "output filename")
+    ut := flag.Float64("ut", float64(1.0), "upper threshold")
+    lt := flag.Float64("lt", float64(0.2), "lower threshold")
+    level := flag.String("level", "strains", "query level")
+    strategy := flag.String("strategy", "oon", "querying strategy")
+    analysis := flag.Bool("analysis", false, "save read query to file")
+    flag.Parse()
+	
+    var f *ppt_filter.Filter
+
+    // Time On
+    defer utils.TimeConsume(time.Now(), "Run time: ")
+
+	// Load filter
+	log.Printf("Load filter")
+    
+    f = ppt_filter.Load(*filter_saved_file)
+    
+    // fmt.Println(f)
+    // fmt.Println(f.Gid)
+    f.Summarize()	
+    log.Println("Finish loading filter.")
+
+    upper_threshold := *ut
+    lower_threshold := *lt
+
+    log.Printf(*out)
+	if *read_2 == "" {
+		f.OnlineSingleQuery(*read_1, *out, *strategy, upper_threshold, lower_threshold, *analysis)	
+	} else {
+        // f.OnlinePairQuery_Threads(*read_1, *read_2, *out, *strategy, upper_threshold, lower_threshold, *analysis, *level)
+        f.OnlinePairQuery_Single(*read_1, *read_2, *out, *strategy, upper_threshold, lower_threshold, *analysis, *level)
+	}
+
+    // print Memory Usage    
+    utils.PrintMemUsage()
+
+}
