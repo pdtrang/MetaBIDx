388321e578e27d268a9f0c496b1a5ab32f3277ad
pdtrang <phamdiemtrang1987@gmail.com>
2023-11-24 15:10:43 -0600

    kmer process

--- src/query/ppt_filter/fastq.go
+++ src/query/ppt_filter/fastq.go
@@ -35,9 +35,9 @@ func (s *FastqScanner) Scan() bool {
             line = s.Scanner.Bytes()
             if len(line)==0 { continue }
             if line[0] == '@' {
-                // s.Header = string(line)
-                s.Header = make([]byte, len(line))
-                copy(s.Header, line)
+                s.Header = line
+                // s.Header = make([]byte, len(line))
+                // copy(s.Header, line)
                 break
             }
         }
@@ -58,9 +58,9 @@ func (s *FastqScanner) Scan() bool {
         line = s.Scanner.Bytes()
         if len(line)==0 { continue }
         if line[0] == '@' {
-            // s.NextHeader = string(line)
-            s.NextHeader = make([]byte, len(line))
-            copy(s.NextHeader, line)
+            s.NextHeader = line
+            // s.NextHeader = make([]byte, len(line))
+            // copy(s.NextHeader, line)
             break
         }
         if line[0] == '+' {
@@ -75,9 +75,9 @@ func (s *FastqScanner) Scan() bool {
        line = s.Scanner.Bytes()
         if len(line)==0 { continue }
         if line[0] == '@' {
-            // s.NextHeader = string(line)
-            s.NextHeader = make([]byte, len(line))
-            copy(s.NextHeader, line)
+            s.NextHeader = line
+            // s.NextHeader = make([]byte, len(line))
+            // copy(s.NextHeader, line)
             break
         }
         if line[0] == '+' {
@@ -90,12 +90,12 @@ func (s *FastqScanner) Scan() bool {
         qual = line
 
     }
-    s.Seq = make([]byte, len(seq))
-    s.Qual = make([]byte, len(qual))
-    copy(s.Seq, seq)
-    copy(s.Qual, qual)
-    // s.Seq = seq
-    // s.Qual = qual
+    // s.Seq = make([]byte, len(seq))
+    // s.Qual = make([]byte, len(qual))
+    // copy(s.Seq, seq)
+    // copy(s.Qual, qual)
+    s.Seq = seq
+    s.Qual = qual
     if err := s.Scanner.Err(); err != nil {
         log.Fatal(err)
     }
