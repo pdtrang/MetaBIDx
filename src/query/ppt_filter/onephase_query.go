3a388e9147c2d8c4d38359e41202350fa1f98c7d
pdtrang <phamdiemtrang1987@gmail.com>
2023-11-25 00:08:16 -0600

    kmer process

--- src/query/ppt_filter/onephase_query.go
+++ src/query/ppt_filter/onephase_query.go
@@ -99,7 +99,7 @@ func (f *FilterInt64) OnePhaseMajorityQueryRead(read []byte, qual []byte, gidx m
 			// 	continue
 			// }
 
-			// fmt.Println("OnePhaseMajQueryRead ", string(read), "   kmer: ", string(kmer_scanner.Kmer), "  kmer_qual: ",string(kmer_scanner.Kmer_qual))
+			fmt.Println("OnePhaseMajQueryRead ", string(read), "   kmer: ", string(kmer_scanner.Kmer), "  kmer_qual: ",string(kmer_scanner.Kmer_qual))
 			// continue query if it is a good kmer
 			kmer_gid, is_valid_kmer = f.OnePhaseQueryHashKmer(kmer_scanner.Kmer, kmer_scanner.Kmer_qual, kmer_qual_threshold)	
 
